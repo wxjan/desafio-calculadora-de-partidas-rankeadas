86d2ea58791f6b530cb15dca4a1abdfafe161503
wxjan <janainasilva.1993@outlook.com>
2023-11-29 21:20:49 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+function calcularNivel(vitorias, derrotas){
+    return vitorias - derrotas
+}
+
+ var saldoVitorias = calcularNivel(vitorias , derrotas)
+ var nivel = ""
+
+if (vitorias < 10 ){
+    nivel = "ferro"
+} else if (vitorias >= 11 && vitorias <= 20 ){
+    nivel = "bronze"
+} else if (vitorias >= 21 &&  vitorias <= 50){
+    nivel = "prata"
+} else if (vitorias >= 51 && vitorias <= 80){
+    nivel = "ouro"
+} else if (vitorias >= 81 && vitorias <= 90){
+    nivel = "diamante"
+} else if (vitorias >= 91 && vitorias <= 100){
+    nivel = "lendario"
+} else {
+    nivel = "imortal"
+}
+
+
+console.log(`O heroi tem saldo de ${saldoVitorias} esta no nivel de ${nivel}`)
